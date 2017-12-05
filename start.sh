e59215a04ba474e4bd1a858e7a6b5a324bdaaaa2
exAclior <rpfzpp@gmail.com>
2017-12-05 14:03:21 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,19 @@ sudo apt-get update
 sudo apt-get -y install g++
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
-sudo pip install Django 
+sudo pip install Django
+sudo apt-get install make
+
+wget http://downloads.sourceforge.net/tcl/tcl8.6.1-src.tar.gz
+sudo tar xzvf tcl8.6.1-src.tar.gz  -C /usr/local/
+cd  /usr/local/tcl8.6.1/unix/
+sudo ./configure
+sudo make
+sudo make install
+cd
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+sudo make install
