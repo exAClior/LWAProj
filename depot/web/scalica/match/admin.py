940a797a405592b9764ea55d3d909f0bb2ec8036
Wentao Zou <wz703@nyu.edu>
2017-12-14 16:14:17 -0500

    Update admin.py

--- depot/web/scalica/match/admin.py
+++ depot/web/scalica/match/admin.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from question.models import *
+from match.models import *
 # Register your models here.
-admin.site.register(Questions)
-admin.site.register(UserAnswer)
+admin.site.register(Match)
