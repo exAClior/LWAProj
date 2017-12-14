7a1dbb9a9b9be86315ca58dd1f80e3b8482b772e
Wentao Zou <wz703@nyu.edu>
2017-12-14 16:01:31 -0500

    Update views.py

--- depot/web/scalica/match/views.py
+++ depot/web/scalica/match/views.py
@@ -8,24 +8,11 @@ from question.models import *
 # Create your views here.
 
 @login_required
-def index(request):
+def match(request):
     if request.user.is_authenticated() or True:
         uid = request.user.id
-        a = UserAnswer.objects.filter(user_id=uid)
-        return render(request, 'index.html', {'answers':a})
+        a = UserList.objects.filter(user_id=uid)
+        return render(request, 'match.html', {'users':a})
     else:
         return  HttpResponseRedirect('/micro/')
-
-
-@login_required
-def match(request):
-    if request.method == "GET" :
-        a = Questions.objects.order_by('?')[:3]
-        return render(request, 'answer.html', {'answers':a})
-    else:
-        na = MatchRequest(user_id = request.user.id)
-        na.save()
-        return HttpResponseRedirect('/match/')
-
-
 
