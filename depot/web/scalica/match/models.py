8e04894a8100539ab41242a48847763b7a846f48
Wentao Zou <wz703@nyu.edu>
2017-12-14 16:07:13 -0500

    Update models.py

--- depot/web/scalica/match/models.py
+++ depot/web/scalica/match/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 
-class MatchResult(models.Model):
+class MatchList(models.Model):
     """
     """
     user_id = models.BigIntegerField(null=False)
