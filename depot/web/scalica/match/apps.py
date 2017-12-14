ebbda461b06223f33fe8da32cb41af31d29f58ee
Wentao Zou <wz703@nyu.edu>
2017-12-14 15:12:41 -0500

    Update apps.py

--- depot/web/scalica/match/apps.py
+++ depot/web/scalica/match/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class QuestionConfig(AppConfig):
-    name = 'question'
+class MatchConfig(AppConfig):
+    name = 'match'
