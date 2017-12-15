1eeb3ec457243276ca655b92c39421b78e72e0ea
Wentao Zou <wz703@nyu.edu>
2017-12-14 20:55:50 -0500

    Update matcher_client.py

--- depot/backend/match_rpc/matcher_client.py
+++ depot/backend/match_rpc/matcher_client.py
@@ -10,12 +10,12 @@ import matcher_pb2_grpc
 def run(userID, userName):
   //The channel need to update to the server IP 
   channel = grpc.insecure_channel('localhost:50051')
-  stub = recommender_pb2_grpc.RecommenderStub(channel)
-  user = recommender_pb2.WhichUser()
+  stub = matcher_pb2_grpc.MatcherStub(channel)
+  user = matcher_pb2.WhichUser()
   user.userID = userID
   user.userName = userName
-  response = stub.Recommend(recommender_pb2.Recommend(user))
-  return response.recommendedID
+  response = stub.Match(matcher_pb2.Match(user))
+  return response.matchID
 
 if __name__ == '__main__':
   run()
